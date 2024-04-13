a12cad841f31885aa9f92dceb25641241ef45f57
Alan <alantan321353@gmail.com>
2024-04-13 12:35:36 +0100

    Added gravity

--- Gmars2/Assets/Scripts/MoveRover.cs
+++ Gmars2/Assets/Scripts/MoveRover.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MoveRover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform planet;
+    public float strength = 3.71f;
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    void Attract()
+    {
+        Vector3 dirOfGrav = transform.position - planet.position;
+        rb.velocity -= dirOfGrav.normalized * strength * Time.deltaTime;
+    }
+
+    void Move()
+    {
+
+    }
+
     void Update()
     {
-        
+        Attract();
     }
+
 }
